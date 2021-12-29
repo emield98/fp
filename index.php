002a26cab0b74b1c321d77cf5f2bb4aac7d9687a
emiel <e.dost.1@student.rug.nl>
2021-12-29 15:24:01 +0100

    css

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Own CSS -->
+    <link rel="stylesheet" href="css/main.css">
+
+    <title>CSS</title>
+</head>
+<body>
+<header>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand">KamersdWeb</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">blabla <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">blabla</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">blabla</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="p-5 text-center bg-image header">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-white text-header">
+                    <h1 class="mb-3">KamerWeb.nl</h1>
+                    <h4 class="mb-3">Voor het laatste aanbod kamers</h4>
+                    <a class="btn btn-outline-light btn-lg" href="#!" role="button">Bekijk kameraanbod</a>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container-fluid">
+    <div class="row row-striped justify-content-md-center">
+        <div class="home">
+            <div class="img-home">
+                <img src="img/huis.png" class="img-fluid" alt="">
+            </div>
+            <div class="text-home">
+                <h3>Woning huren</h3>
+                <p>Bekijk direct het gehele aanbood bij jou in de buurt</p>
+            </div>
+            <div class="btn-home">
+                <button type="button" class="btn btn-info">Huur je woning</button>
+            </div>
+        </div>
+        <div class="home">
+            <div class="img-home">
+                <img src="img/person.png" class="img-fluid" alt="">
+            </div>
+            <div class="text-home">
+                <h3>Vind een makelaar</h3>
+                <p>Bekijk het meest complete overzicht van professionele kantoren en vind een makelaar bij jou in de buurt</p>
+            </div>
+            <div class="btn-home">
+                <button type="button" class="btn btn-info">Naar het overzicht</button>
+            </div>
+
+        </div>
+        <div class="home">
+            <div class="img-home">
+                <img src="img/huis.png" class="img-fluid" alt="">
+            </div>
+            <div class="text-home">
+                <h3>Verhuren</h3>
+                <p>Verhuur je woning snel en gemakkelijk via een makelaar met Kamerweb.nl</p>
+            </div>
+            <div class="btn-home">
+                <button type="button" class="btn btn-info">Ontdek nieuwbouw</button>
+            </div>
+        </div>
+    </div>
+    <div class="row row-striped justify-content-md-center">
+        <div class="col-md-6">
+            <h1>Gratis adverteren!</h1>
+            <p>Ben je op zoek naar een huurder? Zet je woning online en kom direct in contact met studenten, starters en expats.</p>
+        </div>
+    </div>
+</div>
+<div class="footer">
+
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js" integrity="sha512-/DXTXr6nQodMUiq+IUJYCt2PPOUjrHJ9wFrqpJ3XkgPNOZVfMok7cRw6CSxyCQxXn6ozlESsSh1/sMCTF1rL/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js" integrity="sha512-ubuT8Z88WxezgSqf3RLuNi5lmjstiJcyezx34yIU2gAHonIi27Na7atqzUZCOoY4CExaoFumzOsFQ2Ch+I/HCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.min.js" integrity="sha512-UR25UO94eTnCVwjbXozyeVd6ZqpaAE9naiEUBK/A+QDbfSTQFhPGj5lOR6d8tsgbBk84Ggb5A3EkjsOgPRPcKA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
