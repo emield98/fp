345de4bddb78225b493fc8a0fbbddc4727f685dc
emiel <e.dost.1@student.rug.nl>
2022-01-04 13:23:30 +0100

    controller

---
+++ model.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Connects to the database using PDO
+ * @param string $host Database host
+ * @param string $db Database name
+ * @param string $user Database user
+ * @param string $pass Database password
+ * @return PDO Database object
+ */
+function connect_db($host, $db, $user, $pass){
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (PDOException $e) {
+        echo sprintf("Failed to connect. %s",$e->getMessage());
+    }
+    return $pdo;
+}
+
+/**
+ * Check if the route exists
+ * @param string $route_uri URI to be matched
+ * @param string $request_type Request method
+ * @return bool
+ *
+ */
+function new_route($route_uri, $request_type){
+    $route_uri_expl = array_filter(explode('/', $route_uri));
+    $current_path_expl = array_filter(explode('/',parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+    if ($route_uri_expl == $current_path_expl && $_SERVER['REQUEST_METHOD'] == strtoupper($request_type)) {
+        return True;
+    } else {
+        return False;
+    }
+}
+
+/**
+ * Creates a new navigation array item using URL and active status
+ * @param string $url The URL of the navigation item
+ * @param bool $active Set the navigation item to active or inactive
+ * @return array
+ */
+function na($url, $active){
+    return [$url, $active];
+}
+
+/**
+ * Creates filename to the template
+ * @param string $template Filename of the template without extension
+ * @return string
+ */
+function use_template($template){
+    $template_doc = sprintf("views/%s.php", $template);
+    return $template_doc;
+}
+
+/**
+ * Creates breadcrumb HTML code using given array
+ * @param array $breadcrumbs Array with as Key the page name and as Value the corresponding URL
+ * @return string HTML code that represents the breadcrumbs
+ */
+function get_breadcrumbs($breadcrumbs) {
+    $breadcrumbs_exp = '
+    <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">';
+    foreach ($breadcrumbs as $name => $info) {
+        if ($info[1]){
+            $breadcrumbs_exp .= '<li class="breadcrumb-item active" aria-current="page">'.$name.'</li>';
+        }else{
+            $breadcrumbs_exp .= '<li class="breadcrumb-item"><a href="'.$info[0].'">'.$name.'</a></li>';
+        }
+    }
+    $breadcrumbs_exp .= '
+    </ol>
+    </nav>';
+    return $breadcrumbs_exp;
+}
+
+/**
+ * Creates navigation HTML code using given array
+ * @param array $navigation Array with as Key the page name and as Value the corresponding URL
+ * @return string HTML code that represents the navigation
+ */
+function get_navigation($template, $active_id){
+    $navigation_exp = '
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">Series Overview</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">';
+    foreach ($template as $name => $info) {
+        if ($name == $active_id){
+            $navigation_exp .= '<li class="nav-item active">';
+            $navigation_exp .= '<a class="nav-link" href="'.$info['url'].'">'.$info['name'].'</a>';
+        }else{
+            $navigation_exp .= '<li class="nav-item">';
+            $navigation_exp .= '<a class="nav-link" href="'.$info['url'].'">'.$info['name'].'</a>';
+        }
+
+        $navigation_exp .= '</li>';
+    }
+    $navigation_exp .= '
+    </ul>
+    </div>
+    </nav>';
+    return $navigation_exp;
+}
+?>
